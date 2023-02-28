020789064918300938579824ff0e6d5ec8beed31
Mateus Lima <102548678+MateusLaurindo@users.noreply.github.com>
2023-02-28 17:18:58 +0000

    atualização-coffee

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function clicou (){
-  const botaoAbrir = document.getElementById('botao-abrir')
-  const botaoFechar = document.getElementById('botao-fechar')
-
-  if(botaoFechar.innerHTML === `<img src="./img/menu-buguer-close.svg">`){
-    botaoFechar.innerHTML = ''
-  }else{
-          botaoFechar.innerHTML = `<img src="./img/menu-buguer-close.svg">`
-      }
-    botaoAbrir.classList.toggle('abrir')
+function clicou () {
+  const botaoOpen = document.getElementById('botaoOpen')
+  const botaoClose = document.getElementById('botaoClose')
+  const textoSumir = document.querySelector('.letreiro')
+  const listaAberta = document.querySelector('.lista')
+  const footerSumir = document.querySelector('footer')
+  const sumirCoffee = document.querySelector('.coffee-image')
+    
+  if(botaoClose.innerHTML === `<img src="./imagens/menu-buguer-close.svg">`) {
+    botaoClose.innerHTML = ''
+  }else {
+    botaoClose.innerHTML = `<img src="./imagens/menu-buguer-close.svg">`
   }
-  
+  botaoOpen.classList.toggle('abrir');
+  textoSumir.classList.toggle('sumir-texto')
+  listaAberta.classList.toggle('lista-aberta')
+  footerSumir.classList.toggle('footer-sumir')
+  sumirCoffee.classList.toggle('coffee-sumir');
+}
